29ae446c9e08ba9236d37052da5bf984fdae0079
Jeff L <sha0lin@alumni.carnegiemellon.edu>
2014-05-11 17:55:59 -0400

    plot2 done

--- plot2.R
+++ plot2.R
@@ -4,5 +4,7 @@ electric_usage = read.csv2('household_power_consumption.txt')
 electric_usage[, 1] = as.Date(electric_usage[, 1], '%d/%m/%Y')
 subset = electric_usage[as.Date('2007-02-01') <= electric_usage$Date, ]
 subset = subset[subset$Date <= as.Date('2007-02-02'), ]
+subset$datetime = strptime(paste(subset$Date, subset$Time), '%Y-%m-%d %T')
+plot(subset$datetime, subset$Global_active_power, type='l', xlab='', ylab='Global Active Power (kilowatts)')
 dev.copy(png, 'plot2.png', height=480, width=480)
 dev.off()
