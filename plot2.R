44103bcd869a3b9eff294a6ce384d7eb07ff4a3b
Jeff L <sha0lin@alumni.carnegiemellon.edu>
2014-05-11 17:41:04 -0400

    include R code for downloading & unzipping source data file

--- plot2.R
+++ plot2.R
@@ -1,3 +1,5 @@
+download.file('https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip', 'household_power_consumption.zip', 'curl')
+unzip('household_power_consumption.zip')
 electric_usage = read.csv2('household_power_consumption.txt')
 electric_usage[, 1] = as.Date(electric_usage[, 1], '%d/%m/%Y')
 subset = electric_usage[as.Date('2007-02-01') <= electric_usage$Date, ]
